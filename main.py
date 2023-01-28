c5085a9143677baa8b8dc57003aeab37817b41e7
Sampana Abdou Latif <abdoulsa95@gmail.com>
2023-01-28 16:40:48 -0500

    add dialog boxes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
@@ -9,11 +10,20 @@ def save():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
-    with open("data.txt", "a") as data_file:
-        data_file.write(f"{website} | {email} | {password}\n")
-    website_entry.delete(0, END)
-    password_entry.delete(0, END)
-    email_entry.delete(0, END)
+
+    if len(website) == 0 or len(password) == 0 or len(email) == 0:
+        messagebox.showinfo(
+            title="Oops!", message="Please make sure you haven't left anything empty.")
+    else:
+        is_ok = messagebox.askokcancel(
+            title=website, message=f"These are the details entered:\nEmail: {email}\nPassword: {password}\nIs it okay to save")
+
+        if is_ok:
+            with open("data.txt", "a") as data_file:
+                data_file.write(f"{website} | {email} | {password}\n")
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
+                email_entry.delete(0, END)
 
     # ---------------------------- UI SETUP ------------------------------- #
 
